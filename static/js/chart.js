b136d14ae76d1751fce485f986c7c62b132dd561
1173947583 <41962542+1173947583@users.noreply.github.com>
2018-08-02 14:26:15 -0400

    chart
    the bar chart

---
+++ static/js/chart.js
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+<canvas id="myChart"></canvas>
+
+
+</body>
+</html>
+ 
